91f0ebe130889d377c591466138b8dc943a6c1f2
Danhee Kim <sharonkim@g.harvard.edu>
2022-11-06 11:30:28 -0500

    implemented different agents i think

--- mdp.py
+++ mdp.py
@@ -8,23 +8,73 @@ class MDP():
         self.R = R
         self.gamma = gamma
 
+        self.V = np.zeros(len(self.S))
+        self.policy = np.zeros(len(self.S))
+        self.theta = 0.0001
+        self.state = self.S[0]
+
         # sanity checks:
         assert T.shape == (len(self.A), len(self.S), len(self.S)) # action x state x state
         # assert T.sum(axis=0) == 1 # rows of T have to = 1
         assert R.shape == (len(self.S), len(self.A), len(self.S)) # state x action x next_state
 
         # TODO: include value iteration, value per state, Q matrix, optimal policy
+    # implement Q(s, a) = SUM( P(s'|s, a)*[R(s, a, s') + gamma*U(s')] )
+    def updated_action_values(self, state):
+        vals = np.zeros(len(self.A))
 
+        for action in self.A:
+            to_sum = []
+            for p in range(len(self.T[action][state])):
+                to_sum.append(self.T[action][state][p] * (self.R[state][action][p] + (self.gamma * self.V[p])))
+
+            vals[action] = sum(to_sum)
+
+        return vals
+
+    def value_iteration(self):
+        while True:
+            difference = 0
+            for state in self.S:
+                old_V = self.V[state]
+                v = self.updated_action_values(state)
+
+                self.policy[state] = np.argmax(v)
+                self.V[state] = np.max(v)
+
+                difference = max(difference, np.abs(old_V - self.V[state]))
+            if difference < self.theta:
+                break
+
+        print(self.V)
+
+    def solve(self):
+        self.value_iteration()
+
+        if len(self.policy) > 0:
+            grid = []
+            for state in self.S:
+                if self.policy[state] == 0:
+                    grid.append(u'\u2190 ')
+                else:
+                    grid.append(u'\u2192 ')
+
+            toDraw = ''.join(grid)
+            print(toDraw)
+
+        return
+
+    def reset(self):
+        self.state = self.S[0]
 
 ## experiment test -- knob = length of 1d world:
 class Experiment_1D():
 
-    def __init__(self, length):
-        # length is the
-        S, A, T, R, gamma = self.make_MDP_params(length)
-        self.mdp_1d = MDP(S, A, T, R, gamma)
+    def __init__(self, length, make_right_prob):
+        self.S, self.A, self.T, self.R, self.gamma = self.make_MDP_params(length, make_right_prob)
+        self.mdp_1d = MDP(self.S, self.A, self.T, self.R, self.gamma)
 
-    def make_MDP_params(self, length):
+    def make_MDP_params(self, length, make_right_prob):
         S = np.arange(length)
         A = np.array((0, 1)) # 0 is left and 1 is right
         gamma = 0.8
@@ -33,22 +83,39 @@ class Experiment_1D():
         T = np.zeros((2, length, length))
 
         # define T_action == 0 left
-        T[0] = np.diag(np.array([1] + [0.2] * (length-2) + [1])) # 20% chance of staying in the same state unless in state 0 or state l-1
+        T[0] = np.diag(np.array([1] + [1 - make_right_prob] * (length-2) + [1])) # 20% chance of staying in the same state unless in state 0 or state l-1
 
         # Do this more slickly with a np.roll()
         for i in range(1, length-1):
-            T[0, i, i-1] = 0.8
+            T[0, i, i-1] = make_right_prob
 
         T[1] = np.diag(np.array([0.2] * (length-1) + [1])) # 20% chance of staying in the same state unless in state 0 or state l-1
 
         # Do this more slickly with a np.roll()
         for i in range(0, length-1):
-            T[1, i, i+1] = 0.8
+            T[1, i, i+1] = make_right_prob
 
         R = np.zeros((length, 2, length)) # R is sparse
         R[length-2, 1, length-1] = 10
 
         return S, A, T, R, gamma
+
+    def myopic(self, gamma):
+        self.mdp_1d = MDP(self.S, self.A, self.T, self.R, gamma)
+
+    def confident(self, make_right_prob):
+        # probability is LOWER than the "true": UNDERCONFIDENT
+        S, A, T, R, gamma = self.make_MDP_params(length = length, make_right_prob = make_right_prob)
+        self.mdp_1d = MDP(S, A, T, R, gamma)
+
+    def overconfident(self, make_right_prob):
+        # probability is HIGHER than the "true"
+        S, A, T, R, gamma = self.make_MDP_params(make_right_prob)
+        self.mdp_1d = MDP(S, A, T, R, gamma)
+
+    # def reward(self):
+
+
 
 if __name__ == '__main__':
     # example of actually running an exp:
@@ -58,12 +125,36 @@ if __name__ == '__main__':
     # solve mdp, print optimal policy etc.
 
     # checking if implementation is correct
-    test = Experiment_1D(5)
-    print(test.mdp_1d.S)
-    print(test.mdp_1d.A)
-    print(test.mdp_1d.T)
-    print(test.mdp_1d.R[0, 1, 1])
-    print(test.mdp_1d.R[3, 1, 4])
+    length = 5
+    default_prob = 0.8
+    test = Experiment_1D(length, default_prob)
+    # print(test.mdp_1d.S)
+    # print(test.mdp_1d.A)
+    # print(test.mdp_1d.T)
+    # print(test.mdp_1d.R[0, 1, 1])
+    # print(test.mdp_1d.R[3, 1, 4])
+    # myopic = test.myopic(gamma = 0.1)
+    # print(test.mdp_1d.S)
+    # underconfident = test.underconfident(make_right_prob = 0.25)
+    # test.mdp_1d.solve()
+
+    for prob in np.arange(0.01, 1, 0.1):
+        print(f'prob = {prob}')
+        if prob < default_prob:
+            print('UNDERCONFIDENT')
+
+        if prob > default_prob:
+            print('OVERCONFIDENT')
+            
+        confident = test.confident(make_right_prob = prob)
+        test.mdp_1d.solve()
+        print('')
+
+    # for gamma in np.arange(0.01, 1, 0.1):
+    #     print(f'gamma = {gamma}')
+    #     myopic = test.myopic(gamma = gamma)
+    #     test.mdp_1d.solve()
+    #     print('')
 
 
     """
